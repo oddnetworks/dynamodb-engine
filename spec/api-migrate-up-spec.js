a8ad460639cd3405b126c0c88265d1602561252d
Kristoffer Walker <kris@kixx.name>
2016-02-27 23:19:28 -0500

    migrateUp() now adds new indexes to tables.
    	modified:   lib/dynamodb-engine.js
    	modified:   lib/table_schema.js
    	modified:   spec/api-migrate-up-spec.js

--- spec/api-migrate-up-spec.js
+++ spec/api-migrate-up-spec.js
@@ -14,43 +14,77 @@ describe('API migrateUp()', function () {
 	beforeAll(function (done) {
 		constants = this.constants;
 
-		var db = DynamoDBEngine.create({
-			accessKeyId: constants.AWS_ACCESS_KEY_ID,
-			secretAccessKey: constants.AWS_SECRET_ACCESS_KEY,
-			region: constants.AWS_REGION,
-			endpoint: constants.DYNAMODB_ENDPOINT,
-			tablePrefix: constants.TABLE_PREFIX
-		}, {
-			Character: {},
-			Series: {}
-		});
+		var args = new Map(constants);
 
-		var args = U.constant(new Map(U.extend(
-			Object.create(null),
-			{db: db},
-			constants
-		)));
+		// Initial DB schema without indexes.
+		args = args.set('db', createDbInstance(
+			constants,
+			{Character: {}, Series: {}}
+		));
 
-		Promise.resolve(args())
+		function returnArgs() {
+			return args;
+		}
+
+		Promise.resolve(args)
+
+			// Clean up from previous tests.
 			.then(removeTestTables)
-			.then(args)
+			.then(returnArgs)
 			.then(listTestTables)
 			.then(function (res) {
 				subject = subject.set('initialTableList', new List(res));
 			})
-			.then(args)
+
+			// Run the first migrateUp() without indexes.
+			.then(returnArgs)
 			.then(migrateUp)
-			.then(args)
+			.then(returnArgs)
 			.then(describeTestTables)
 			.then(function (res) {
 				subject = subject.set('tables', Immutable.fromJS(res));
 			})
+
+			// Run the second migrateUp() adding new indexes.
 			.then(function () {
-				Object.freeze(subject);
+				args = args.set('db', createDbInstance(
+					constants,
+					{
+						Character: {
+							indexes: {
+								ByName: {
+									keys: {
+										hash: {name: 'type', type: 'String'},
+										range: {name: 'name', type: 'String'}
+									}
+								}
+							}
+						},
+						Series: {
+							indexes: {
+								ByTitle: {
+									keys: {
+										hash: {name: 'type', type: 'String'},
+										range: {name: 'title', type: 'String'}
+									}
+								}
+							}
+						}
+					}
+				));
 			})
+			.then(returnArgs)
+			.then(migrateUp)
+			.then(returnArgs)
+			.then(describeTestTables)
+			.then(function (res) {
+				subject = subject.set('withIndexes', Immutable.fromJS(res));
+			})
+
+			// Complete setup
 			.then(U.returnUndefined)
 			.then(done, done.fail);
-	}, 100 * 1000); // Allow setup to run longer
+	}, 120 * 1000); // Allow setup to run longer
 
 	it('initializes with no tables', function () {
 		var list = subject.get('initialTableList');
@@ -115,12 +149,9 @@ describe('API migrateUp()', function () {
 		expect(tables.length).toBe(1);
 
 		var desc = tables[0];
-		var KeySchema = desc.Table.KeySchema.sort(sort);
-		var AttributeDefinitions = desc.Table.AttributeDefinitions.sort(sort);
-
-		function sort(a, b) {
-			return a.AttributeName > b.AttributeName ? 1 : -1;
-		}
+		var KeySchema = desc.Table.KeySchema.sort(sortByAttributeName);
+		var AttributeDefinitions = desc.Table.AttributeDefinitions
+			.sort(sortByAttributeName);
 
 		expect(KeySchema[0])
 			.toEqual({AttributeName: 'object', KeyType: 'RANGE'});
@@ -164,7 +195,120 @@ describe('API migrateUp()', function () {
 
 		expect(indexStatus).toEqual(['ACTIVE', 'ACTIVE']);
 	});
+
+	describe('after indexes added', function () {
+		function mapGsi(gsiDesc) {
+			var KeySchema = gsiDesc.KeySchema.sort(sortByAttributeName);
+			return {
+				IndexName: gsiDesc.IndexName,
+				KeySchema: KeySchema
+			};
+		}
+
+		beforeAll(function () {
+			// Map the AWS responses into something we can more easily test.
+			this.tables = subject.get('withIndexes')
+				.toJS()
+				.map(function (desc) {
+					var AttributeDefinitions = desc.Table.AttributeDefinitions
+						.sort(sortByAttributeName);
+
+					var GSI = desc.Table.GlobalSecondaryIndexes.map(mapGsi);
+
+					return {
+						TableName: desc.Table.TableName,
+						AttributeDefinitions: AttributeDefinitions,
+						GlobalSecondaryIndexes: GSI
+					};
+				})
+				.sort(function (a, b) {
+					return a.TableName > b.TableName ? 1 : -1;
+				});
+		});
+
+		it('returns the correct tables', function () {
+			var tableNames = this.tables
+				.filter(function (desc) {
+					return /entities$/.test(desc.TableName);
+				})
+				.map(function (desc) {
+					return desc.TableName;
+				});
+
+			expect(tableNames).toEqual([
+				'ddb_engine_tests_character_entities',
+				'ddb_engine_tests_series_entities'
+			]);
+		});
+
+		it('correctly names the indexes', function () {
+			var indexNames = this.tables.reduce(function (indexNames, desc) {
+				return indexNames.concat(
+					desc.GlobalSecondaryIndexes.map(pluckIndexName)
+				);
+			}, []);
+
+			function pluckIndexName(gsi) {
+				return gsi.IndexName;
+			}
+
+			expect(indexNames).toEqual([
+				'ddb_engine_tests_character_by_name',
+				'ddb_engine_tests_has_many',
+				'ddb_engine_tests_belongs_to',
+				'ddb_engine_tests_series_by_title'
+			]);
+		});
+
+		it('has the required AttributeDefinitions', function () {
+			var defs = this.tables
+				.filter(function (desc) {
+					return /entities$/.test(desc.TableName);
+				})
+				.reduce(function (defs, desc) {
+					return defs.concat(desc.AttributeDefinitions);
+				}, []);
+
+			expect(defs[0]).toEqual({AttributeName: 'id', AttributeType: 'S'});
+			expect(defs[1]).toEqual({AttributeName: 'name', AttributeType: 'S'});
+			expect(defs[2]).toEqual({AttributeName: 'type', AttributeType: 'S'});
+			expect(defs[3]).toEqual({AttributeName: 'id', AttributeType: 'S'});
+			expect(defs[4]).toEqual({AttributeName: 'title', AttributeType: 'S'});
+			expect(defs[5]).toEqual({AttributeName: 'type', AttributeType: 'S'});
+		});
+
+		it('has the correct KeySchema', function () {
+			var keys = this.tables
+				.filter(function (desc) {
+					return /entities$/.test(desc.TableName);
+				})
+				.reduce(function (keys, desc) {
+					return keys.concat(
+						U.flatten(desc.GlobalSecondaryIndexes.map(pluckKeySchema))
+					);
+				}, []);
+
+			function pluckKeySchema(gsi) {
+				return gsi.KeySchema;
+			}
+
+			expect(keys[0]).toEqual({AttributeName: 'name', KeyType: 'RANGE'});
+			expect(keys[1]).toEqual({AttributeName: 'type', KeyType: 'HASH'});
+			expect(keys[2]).toEqual({AttributeName: 'title', KeyType: 'RANGE'});
+			expect(keys[3]).toEqual({AttributeName: 'type', KeyType: 'HASH'});
+		});
+	});
 });
+
+function createDbInstance(constants, schema) {
+	return DynamoDBEngine.create({
+		accessKeyId: constants.AWS_ACCESS_KEY_ID,
+		secretAccessKey: constants.AWS_SECRET_ACCESS_KEY,
+		region: constants.AWS_REGION,
+		endpoint: constants.DYNAMODB_ENDPOINT,
+		tablePrefix: constants.TABLE_PREFIX
+	}, schema);
+}
 
 function removeTestTables(args) {
 	return listTestTables(args).then(function (tableNames) {
@@ -205,4 +349,8 @@ function describeTestTables(args) {
 			return dynamodb.describeTable({TableName: tableName});
 		}));
 	});
+}
+
+function sortByAttributeName(a, b) {
+	return a.AttributeName > b.AttributeName ? 1 : -1;
 }
